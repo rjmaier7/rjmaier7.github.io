fe22f4892fe53d8681a941e0434c59ce2319d9df
rjmaier7 <69917279+rjmaier7@users.noreply.github.com>
2021-09-25 13:20:59 +0200

    Add files via upload

---
+++ assets/js/raphaels_scripts.js
@@ -0,0 +1,25 @@
+
+  <!-- cookies -->
+  <!-- Cookie Consent by https://www.PrivacyPolicies.com -->
+ <script type="text/javascript" src="//www.privacypolicies.com/public/cookie-consent/4.0.0/cookie-consent.js" charset="UTF-8"></script>
+ <script type="text/javascript" charset="UTF-8">
+ document.addEventListener('DOMContentLoaded', function () {
+ cookieconsent.run({"notice_banner_type":"simple","consent_type":"implied","palette":"light","language":"de","page_load_consent_levels":["strictly-necessary","functionality","tracking","targeting"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false});
+ });
+ </script>
+ 
+ <!-- Google Analytics -->
+ <!-- Google Analytics -->
+ <script type="text/plain" cookie-consent="tracking">
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ 
+ ga('create', 'UA-XXXXX-Y', 'auto');
+ ga('send', 'pageview');
+ </script>
+ <!-- End Google Analytics -->
+ <!-- end of Google Analytics-->
+ 
+ <noscript>Cookie Consent by <a href="https://www.PrivacyPolicies.com/cookie-consent/" rel="nofollow noopener">PrivacyPolicies.com</a></noscript>
